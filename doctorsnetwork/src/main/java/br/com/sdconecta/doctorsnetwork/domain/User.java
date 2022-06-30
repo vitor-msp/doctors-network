b073710721f88b0e3ce2c18f8faa15af7a5d1ed0
Vitor Mateus <vitor7jan@gmail.com>
2022-06-30 19:35:50 -0300

    user domain: fix relation with crm

--- doctorsnetwork/src/main/java/br/com/sdconecta/doctorsnetwork/domain/User.java
+++ doctorsnetwork/src/main/java/br/com/sdconecta/doctorsnetwork/domain/User.java
@@ -1,10 +1,15 @@
 package br.com.sdconecta.doctorsnetwork.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -37,6 +42,21 @@ public class User {
 	@NotBlank
 	@Size(max = 255)
 	private String mobilePhone;
+	
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Crm> crms;
+	
+	public User() {}
+	
+	public User(@NotBlank @Size(max = 255) String email, @NotBlank @Size(max = 255) String password,
+			@NotBlank @Size(max = 255) String name, @NotBlank @Size(max = 255) String surname,
+			@NotBlank @Size(max = 255) String mobilePhone) {
+		this.email = email;
+		this.password = password;
+		this.name = name;
+		this.surname = surname;
+		this.mobilePhone = mobilePhone;
+	}
 
 	public int getId() {
 		return id;
@@ -85,5 +105,12 @@ public class User {
 	public void setMobilePhone(String mobilePhone) {
 		this.mobilePhone = mobilePhone;
 	}
-	
+
+	public List<Crm> getCrms() {
+		return crms;
+	}
+
+	public void setCrms(List<Crm> crms) {
+		this.crms = crms;
+	}
 }
