693bd6947c0e39e4938d0a15bb45a9f0337dbd40
Vitor Mateus <vitor7jan@gmail.com>
2022-06-29 21:53:39 -0300

    initial commit: basic spring configuration

---
+++ doctorsnetwork/src/main/java/br/com/sdconecta/doctorsnetwork/DoctorsNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.com.sdconecta.doctorsnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsNetworkApplication.class, args);
+	}
+
+}
