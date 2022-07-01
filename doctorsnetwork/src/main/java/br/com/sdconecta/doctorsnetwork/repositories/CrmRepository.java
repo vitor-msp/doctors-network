e0584e7f3fdd9bed767d507220edce48b6955233
Vitor Mateus <vitor7jan@gmail.com>
2022-07-01 00:23:36 -0300

    clean crmrepository

--- doctorsnetwork/src/main/java/br/com/sdconecta/doctorsnetwork/repositories/CrmRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sdconecta.doctorsnetwork.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.sdconecta.doctorsnetwork.domain.Crm;
-
-public interface CrmRepository extends JpaRepository<Crm, Integer>{
-
-}
